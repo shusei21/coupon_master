2804b0af159aef1222a258520d1e41b98fda4d50
山下秀星 <yamashitashuusei@Shuseis-MacBook-Pro.local>
2021-12-23 16:37:23 +0900

    make it better

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'http://cpmaster.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
