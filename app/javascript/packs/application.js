aeb97b786967168dbd3e3acc67e9e237fe825ad4
shusei_yamashita <yamashita.shusei@gmail.com>
2023-10-25 19:59:57 +0900

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
-import '../src/application.scss'
-import '@fortawesome/fontawesome-free/js/all'
-
-require("jquery")
+import "jquery"
 
 Rails.start()
 Turbolinks.start()
